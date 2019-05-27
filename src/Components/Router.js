fc5ccbb3742f10050714ae66ad6da30e405b159d
Kang.Yuchan <ikimaku0517@gmail.com>
2019-05-27 22:24:50 +0900

    Styling with styled-components

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {BrowserRouter as Router, Route, Redirect, Switch} from "react-router-dom";
+import Header from "Components/Header";
 import Home from "Routes/Home";
 import Search from "Routes/Search";
 import TV from "Routes/TV";
@@ -9,13 +10,16 @@ import Detail from "Routes/Detail"
 
 export default () => (
     <Router>
-    <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/search" exact component={Search} />
-        <Route path="/tv" exact component={TV} />
-        <Route path="/tv/popular" render={() => <h1>Popular</h1>} />
-        <Route path="/detail" exact component={Detail} />
-        <Redirect from="*" to="/" />
-    </Switch>
+    <>
+        <Header />
+        <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/search" exact component={Search} />
+            <Route path="/tv" exact component={TV} />
+            <Route path="/tv/popular" render={() => <h1>Popular</h1>} />
+            <Route path="/detail" exact component={Detail} />
+            <Redirect from="*" to="/" />
+        </Switch>
+    </>
     </Router>
 );
