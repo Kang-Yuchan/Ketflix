3f950e80fa69573588d4183f7b68ea239258e0ba
Kang.Yuchan <ikimaku0517@gmail.com>
2019-05-27 22:52:55 +0900

    global and Header styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,39 +3,50 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Header = styled.header `
-
+    color: white;
+    position: fixed;
+    top:0;
+    left:0;
+    width: 100%;
+    height: 50px;
+    display:flex;
+    align-items: center;
+    padding:0px 10px;
 `;
 
 const List = styled.ul`
     display:flex;
-    &:hover {
-        background-color:blue;
-    }
+   
 `;
 
 const Item = styled.li `
-
+    width:70px;
+    height: 50px;
+    text-align:center;
 `;
 
 const SLink = styled(Link)`
-
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
 
 export default () => (
     <Header>
         <List>
-            <li>
-                <SLink to="/">Movies</SLink>
-            </li>
-            <li>
-                <SLink to="/tv">TV</SLink>
-            </li>
-            <li>
-                <SLink to="/search">Search</SLink>
-            </li>
-            <li>
-                <SLink to="/detail">Detail</SLink>
-            </li>
+            <Item>
+                <SLink to="/">ホーム</SLink>
+            </Item>
+            <Item>
+                <SLink to="/tv">TV番組</SLink>
+            </Item>
+            <Item>
+                <SLink to="/search">検索</SLink>
+            </Item>
+            <Item>
+                <SLink to="/detail">詳細</SLink>
+            </Item>
         </List>
     </Header>
 );
