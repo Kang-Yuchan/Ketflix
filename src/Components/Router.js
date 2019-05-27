f5352a2f0ed05c565de033a26cd2dfadb15a5dc9
Kang.Yuchan <ikimaku0517@gmail.com>
2019-05-27 21:48:18 +0900

    React Router

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,15 +1,21 @@
 import React from "react";
-import {HashRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Redirect, Switch} from "react-router-dom";
 import Home from "Routes/Home";
 import Search from "Routes/Search";
 import TV from "Routes/TV";
-import Detail from "Routes/Detail";
+import Detail from "Routes/Detail"
+
+
 
 export default () => (
     <Router>
+    <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/search" exact component={Search} />
         <Route path="/tv" exact component={TV} />
+        <Route path="/tv/popular" render={() => <h1>Popular</h1>} />
         <Route path="/detail" exact component={Detail} />
+        <Redirect from="*" to="/" />
+    </Switch>
     </Router>
 );
