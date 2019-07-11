822f8d49ceb8b1178c4c927fcc940ada23fd9b1e
Kang.Yuchan <ikimaku0517@gmail.com>
2019-07-11 23:14:29 +0900

    fix Error text

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -80,7 +80,7 @@ const SearchPresenter = ({
           movieResults &&
           tvResults.length === 0 &&
           movieResults.length === 0 && (
-            <Message text="Nothing found" color="#95a5a6" />
+            <Message text="検索結果が見つかりません。" color="#95a5a6" />
           )}
       </>
     )}
