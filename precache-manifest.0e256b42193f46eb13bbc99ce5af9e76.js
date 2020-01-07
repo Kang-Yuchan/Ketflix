e7cc8127bf0800930030dbec7911ab87b40bed76
Kang.Yuchan <ikimaku0517@gmail.com>
2020-01-07 11:38:49 +0900

    Updates

---
+++ precache-manifest.0e256b42193f46eb13bbc99ce5af9e76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd110e4dad768168e8f21ce19ff5c09",
+    "url": "/Ketflix/index.html"
+  },
+  {
+    "revision": "96a8bf49b15cba23e0ea",
+    "url": "/Ketflix/static/js/2.a8a27dee.chunk.js"
+  },
+  {
+    "revision": "da534d96cdc919f1bedd",
+    "url": "/Ketflix/static/js/main.f941258b.chunk.js"
+  },
+  {
+    "revision": "15944323538d132ec859",
+    "url": "/Ketflix/static/js/runtime~main.cd484ce0.js"
+  },
+  {
+    "revision": "5fcc7d93433f3a81e6d216854a92965e",
+    "url": "/Ketflix/static/media/default.5fcc7d93.png"
+  }
+]);
