3972ba2d7d60ba7077cd294ad717620a204f9b9f
Kang.Yuchan <ikimaku0517@gmail.com>
2020-02-24 20:58:26 +0900

    delete comment code

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -3,61 +3,6 @@ import DetailPresenter from "./DetailPresenter";
 import { moviesApi, tvApi } from "api";
 
 
-//export default class extends React.Component {
-//    constructor(props) {
-//        super(props);
-//        const {
-//            location: { pathname }
-//        } = props;
-//        this.state = {
-//            result: null,
-//            error: null,
-//            loading: true,
-//            isMovie: pathname.includes("/movie/")
-//        };
-//    }
-//
-//    async componentDidMount() {
-//        const { 
-//            match: {
-//                params: { id }
-//            },
-//            history: { push }
-//        } = this.props;
-//        const { isMovie } = this.state;
-//        const parsedId = parseInt(id);
-//        if(isNaN(parsedId)) {
-//            return push("/");
-//        }
-//        let result = null;
-//        try {
-//            if(isMovie) {
-//                ({ data: result } = await moviesApi.movieDetail(parsedId));
-//            } else {
-//                ({ data: result } = await tvApi.showDetail(parsedId));
-//            }
-//          
-//        } catch {
-//            this.setState({error: "該当する検索結果がありません。"})
-//        } finally {
-//            this.setState({ loading: false, result });
-//        }
-//    }
-//    
-//
-//    render () {
-//        const {result, error, loading} = this.state;
-//        return  (
-//        <DetailPresenter 
-//            result={result}
-//            error={error}
-//            loading={loading}
-//        />
-//
-//        );
-//    }    
-//}
-
 const Detail = (props) => {
     const { location : { pathname } } = props;
     const [result, setResult] = useState(null);
